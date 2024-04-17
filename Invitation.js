341f5aac47e2e415f0ae0b7486cc77c31ccb8f1d
Vinit More <viniot1027@gmail.com>
2024-04-17 20:46:44 +0530

    this is the first copy

---
+++ Invitation.js
@@ -0,0 +1,32 @@
+function event1(){
+    var t = document.getElementById('in1').value;
+    document.getElementById('one').innerHTML = t;
+}
+
+
+function event2(){
+    var x = document.getElementById('in2').value;
+    document.getElementById('two').innerHTML = x;
+}
+
+
+function event3(){
+    var y = document.getElementById('in3').value;
+    document.getElementById('three').innerHTML = y;
+}
+
+
+function event4(){
+    var z = document.getElementById('in4').value;
+    document.getElementById('four').innerHTML = z;
+}
+
+function event5(){
+    var z = document.getElementById('in5').value;
+    document.getElementById('five').innerHTML = z;
+}
+
+function event6(){
+    var z = document.getElementById('in6').value;
+    document.getElementById('six').innerHTML = z;
+}
